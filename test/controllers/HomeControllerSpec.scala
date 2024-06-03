22770ef6149113c8103a9044a21496b7b679afbb
Mateusz Ciochoń <mateusz.ciochon.23@gmail.com>
2024-06-03 20:35:39 +0200

    fix v2

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -13,13 +13,13 @@ import play.api.test.Helpers._
  */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
+  val welcome = "Welcome to Play";
+  val th = "text/html"
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
       val controller = new HomeController(stubControllerComponents())
       val home = controller.index().apply(FakeRequest(GET, "/"))
-      val welcome = "Welcome to Play";
-      val th = "text/html"
 
       status(home) mustBe OK
       contentType(home) mustBe Some(th)
